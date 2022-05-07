b65b7ea92c2c61194559385b73fa7aceb4c79146
Andrea Scuderi <andreascuderi@ymail.com>
2022-05-07 06:59:31 +0100

    Initial Commit

---
+++ DemoTuist/DemoTuistApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTuistApp.swift
+//  DemoTuist
+//
+//  Created by Andrea Scuderi on 07/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct DemoTuistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
